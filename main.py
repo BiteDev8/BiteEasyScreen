d1f1933160dca3c61ce45760931cc7be1d62d684
BiteDev <113308968+BiteDev8@users.noreply.github.com>
2022-09-19 21:35:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,45 @@
-import pyscreenshot 
+from colorama import Fore, Back, Style, init
 from discord_webhook import DiscordWebhook
+import pyscreenshot 
 import schedule
 import keyboard
-from colorama import Fore, Back, Style, init
 import pyfiglet
-
+import json
 T = "BiteEasyScreen"
 ASCII_art_1 = pyfiglet.figlet_format(T)
 print(ASCII_art_1)
 init()
-
 print(Fore.GREEN+"")
-webhook = str(input("met ici ton webhook :"))
-Bite = str(input("Choisis ton raccourci en format ecrit (exemple : ctrl+alt+x): \n"))
+sauvegarde = ""
+question_de_import = str(input("importer vos sauvegarde ?(n si c'est votre premiere fois)  y/n :"))
+if question_de_import == "y" :    
+    with open("data.json", "r") as f:
+        data = json.load(f)
+        raccourcie = data["raccourcie"]
+        webhook = data["webhook"]
+else :
+    webhook =str(input("met ici ton webhook :"))
+    raccourcie = str(input("Choisis ton raccourci en format ecrit (exemple : ctrl+alt+x): \n"))
+    sauvegarde = str(input("sauvergardez les parametres ?  y/n :"))
+webhook = str(webhook)
+raccourcie = str(raccourcie)
+if sauvegarde == "y" :
+    Sauvegardejson = {"webhook" : webhook,
+    "raccourcie" : raccourcie
+    }
+
+    with open("data.json", "w") as json_file:
+        json.dump(Sauvegardejson, json_file)
+    print("sauvegarde reussi")    
 print(Fore.YELLOW+"En fonctionnement ! utilises ton raccourci pour recevoir ton ecran sur discord !")
 print(Fore.RED+"alt+f4 ou ctrl+c pour arreter")
-
 while True :
-    keyboard.wait(Bite)
+    keyboard.wait(raccourcie)
     x = "Noupi"  
     if x == "Noupi" :
+        with open("data.json", "r") as f:
+            data = json.load(f)
+        webhook = data["webhook"]
         image = pyscreenshot.grab()  
         image.save("BiteScreen.png")
         webhook = DiscordWebhook(url=webhook, username="BiteScreen")
@@ -27,7 +47,8 @@ while True :
             webhook.add_file(file=f.read(), filename='BiteScreen.png')
         with open("BiteScreen.png", "rb") as f:
             webhook.add_file(file=f.read(), filename='BiteScreen.png')
+        with open("BiteScreen.png", "rb") as f:
+            webhook.add_file(file=f.read(), filename='BiteScreen.png')
+        with open("BiteScreen.png", "rb") as f:
+            webhook.add_file(file=f.read(), filename='BiteScreen.png')
         response = webhook.execute()
-    else :
-        pass
-    
